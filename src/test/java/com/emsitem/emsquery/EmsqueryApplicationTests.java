50622837643aa92fa465145ed7962e551d617653
姜文埼煜 <1779999@qq.com>
2022-05-11 19:05:43 +0800

    初始化提交

---
+++ src/test/java/com/emsitem/emsquery/EmsqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsitem.emsquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
